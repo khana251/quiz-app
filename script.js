444b56defa636a926fd1a041038a14153c8d61f5
amaan-k <59571647+amaan-k@users.noreply.github.com>
2023-05-18 22:38:46 -0400

    Adding base files

---
+++ script.js
@@ -0,0 +1,10 @@
+const quizData = [
+    {
+        question: 'How can you check your current git version?',
+        a1: 'git --v',
+        a2: 'git --version',
+        a3: 'git --option',
+        correct: 'git --version'
+    }
+    
+]
